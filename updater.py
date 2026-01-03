069254b2400ad765645bcaeb30f9318f22e849f0
Derek <derek@example.com>
2026-01-02 21:15:31 -0800

    Bump version to v1.1.1 for auto-update release

--- updater.py
+++ updater.py
@@ -7,7 +7,7 @@ import shutil
 from packaging import version
 
 # Current version of the application
-CURRENT_VERSION = "v1.1.0"
+CURRENT_VERSION = "v1.1.1"
 
 # GitHub Repository details
 REPO_OWNER = "dlanz1"
